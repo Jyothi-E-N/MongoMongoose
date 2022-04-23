0d6276fe96bd65d293403de4fd2ea30d60c690cc
Jyothi-E-N <jyothienarendramath252@gmail.com>
2022-04-23 14:44:15 +0530

    Query chain the methods

--- myApp.js
+++ myApp.js
@@ -109,8 +109,14 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
     const foodToSearch = "burrito";
-
-    done(null /*, data*/);
+    Person.find({ favoriteFoods: [foodToSearch] })
+        .sort({ name: "asc" })
+        .limit(2)
+        .select(-age)
+        .exec((err, data) => {
+            if (err) return console.log(err);
+            done(null, data);
+        });
 };
 
 /** **Well Done !!**
